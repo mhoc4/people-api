a97de2759092f952b7310210480b7e650eab8303
mhoc4 <acmjpf@gmail.com>
2021-07-25 21:08:46 -0300

    initial commit

---
+++ src/main/java/com/github/mhoc4/PersonManagerAPI/PersonManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.mhoc4.PersonManagerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/mhoc4/PersonManagerAPI/PersonManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mhoc4.PersonManagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
